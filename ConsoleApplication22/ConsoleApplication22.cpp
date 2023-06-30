b8758a3570612d9020a096ed5c5c663c1a21ff1b
User1 <example@gmail.com>
2023-06-30 20:17:57 +0300

    class AppUI, lambdas

--- ConsoleApplication22/ConsoleApplication22.cpp
+++ ConsoleApplication22/ConsoleApplication22.cpp
@@ -1,46 +1,73 @@
 #include <iostream>
 #include <string>
+#include <unordered_map>
+#include <functional>
 #include "Storage.h"
 #include "Entry.h"
 
 using namespace std;
 
+class AppUI;
 
+typedef function<void(AppUI*, Storage&)> appfunc;
 
-// Консольный интерфейс для создания записей
-Entry* create_entry() {
-    string name;
-    string text;
-
-    cout << "Enter name: ";
-    getline(cin, name);
-
-    cout << "Text: ";
-    getline(cin, text);
-
-    // Получаем время создания записи
-    time_t created = time(0);
-    return new Entry(name, text, created);
+void test(AppUI* app, Storage& storage) {
+    cout << "Hello, World" << endl;
 }
 
+class AppUI {
+    // Консольный интерфейс для создания записей
+    Entry* create_entry() {
+        string name;
+        string text;
 
-void printGuide() {
-    cout << "Доступные команды:" << endl;
-    cout << "  list   - список сохранённых записей" << endl;
-    cout << "  add    - создать запись" << endl;
-    cout << "  show   - показать запись" << endl;
-    cout << "  delete - удалить запись" << endl;
-    cout << "  exit   - завершить программу" << endl;
-}
+        cout << "Enter name: ";
+        getline(cin, name);
 
-void mainloop(Storage& storage) {
-    printGuide();
-    while (true) {
-        string cmd;
-        cout << "> ";
-        getline(cin, cmd);
+        cout << "Text: ";
+        getline(cin, text);
+
+        // Получаем время создания записи
+        time_t created = time(0);
+        return new Entry(name, text, created);
     }
-}
+    void printGuide() {
+        cout << "Доступные команды:" << endl;
+        cout << "  list   - список сохранённых записей" << endl;
+        cout << "  add    - создать запись" << endl;
+        cout << "  show   - показать запись" << endl;
+        cout << "  delete - удалить запись" << endl;
+        cout << "  exit   - завершить программу" << endl;
+    }
+
+    Storage& storage;
+    unordered_map<string, appfunc> functions;
+public:
+    AppUI(Storage& storage) : storage(storage) {
+        functions["test"] = test;
+        int a = 42;
+        functions["test2"] = [a](AppUI* app, Storage& storage) {
+            cout << "Test2 " << a << endl;
+        };
+    }
+
+    void mainloop() {
+        printGuide();
+        while (true) {
+            string cmd;
+            cout << "> ";
+            getline(cin, cmd);
+
+            auto found = functions.find(cmd);
+            if (found != functions.end()) {
+                found->second(this, storage);
+            }
+            else {
+                cout << "такой команды не существует" << endl;
+            }
+        }
+    }
+};
 
 
 int main() {
@@ -51,7 +78,8 @@ int main() {
     Storage storage("database.txt");
     storage.load();
 
-    mainloop(storage);
+    AppUI app(storage);
+    app.mainloop();
 
     // Сохраняем записи в файл
     storage.save();
